e3afa05e7b3374d54140772be0c1fa1913833a55
rayallasureshkumar <rayallasureshkumar@gmail.com>
2015-11-17 15:39:32 +0530

    saffschool

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151117091030) do
+ActiveRecord::Schema.define(:version => 20151117100230) do
+
+  create_table "schools", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
